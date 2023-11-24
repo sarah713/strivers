59bff5c78bcea976948d78b0605ca6760f3078b0
Sarah <sarahkulsoom18@gmail.com>
2023-11-24 17:45:55 +0530

    Contains Duplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
