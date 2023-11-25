6152a838bcb873265938fbfcb739464b68d1eae9
Sarah <sarahkulsoom18@gmail.com>
2023-11-25 14:37:44 +0530

    Search In rotated sorted array

---
+++ SearchInRotatedSortedarr.java
@@ -0,0 +1,26 @@
+public class SearchInRotatedSortedarr {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[lo]) {
+                if (nums[lo] <= target && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
