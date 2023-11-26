5754650ec214fabb5c0b37b9a22e3c750ef74774
Sarah <sarahkulsoom18@gmail.com>
2023-11-26 13:01:41 +0530

    Reverse linked list

---
+++ ReverseLinkedList.java
@@ -0,0 +1,36 @@
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// head
+// |
+// 1 -> 2 -> 3 -> 4 -> 5 -> null
+// 5 -> 4 -> 3 -> 2 -> 1 -> null
