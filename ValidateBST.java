834315a9ac8058841768bda54a4e2e5d70d814f9
Sarah <sarahkulsoom18@gmail.com>
2023-11-28 23:04:12 +0530

    Validate BST

---
+++ ValidateBST.java
@@ -0,0 +1,18 @@
+public class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val < min || root.val > max) {
+            return false;
+        }
+        return isBST(root.left, min, root.val) && isBST(root.right, root.val, max);
+    }
+}
