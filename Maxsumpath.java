5e22ed374bdc999caefe89905d3a50d7185ccde8
Sarah <sarahkulsoom18@gmail.com>
2023-11-30 21:39:26 +0530

    max path sum

---
+++ Maxsumpath.java
@@ -0,0 +1,25 @@
+public class Maxsumpath {
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        helper(root);
+        return maxSum;
+    }
+
+    public int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = Math.max(helper(root.left), 0);
+        int rightSum = Math.max(helper(root.right), 0);
+        int sum = root.val + leftSum + maxSum;
+        maxSum = Math.max(maxSum, sum);
+
+        return root.val + Math.max(rightSum, leftSum);
+    }
+
+}
