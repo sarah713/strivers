26d803cc585f0de72b8f6dad6c6f880509132316
Sarah <sarahkulsoom18@gmail.com>
2023-12-07 23:15:44 +0530

    House Robber

---
+++ HouseRobber.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int take = nums[i];
+            if (i > 1) {
+                take += dp[i - 2];
+            }
+            int dont = dp[i - 1];
+
+            dp[i] = Math.max(take, dont);
+        }
+        return dp[nums.length - 1];
+    }
+}
