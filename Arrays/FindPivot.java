03f59cf76cec073cd2b566dc06d935557c14dff7
Sarah <sarahkulsoom18@gmail.com>
2023-12-25 19:01:56 +0530

    Find Pivot Index

---
+++ Arrays/FindPivot.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class FindPivot {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0, sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == sum - leftSum - nums[i]) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
