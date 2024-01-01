df5b4a1403e0396cb2b046bc4b15cad35446b75b
Sarah <sarahkulsoom18@gmail.com>
2024-01-01 23:20:53 +0530

    Sort colors

---
+++ SortColors.java
@@ -0,0 +1,21 @@
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int zero = 0, one = 0, two = nums.length - 1;
+        while (one < two) {
+            if (nums[one] == 0) {
+                int tmp = nums[one];
+                nums[one] = nums[zero];
+                nums[zero] = tmp;
+                one++;
+                zero++;
+            } else if (nums[one] == two) {
+                int tmp = nums[one];
+                nums[one] = nums[two];
+                nums[two] = tmp;
+                two--;
+            } else {
+                one++;
+            }
+        }
+    }
+}
