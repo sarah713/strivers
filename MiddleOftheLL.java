10b5c2f9595d09329515fc8bba482fdf5c5814ef
Sarah <sarahkulsoom18@gmail.com>
2024-01-03 21:38:33 +0530

    Middle of the linkedlist

---
+++ MiddleOftheLL.java
@@ -0,0 +1,13 @@
+public class MiddleOftheLL {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
