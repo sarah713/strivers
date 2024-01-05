455764d0607c22163a1a5dcd6694f797678a7230
Sarah <sarahkulsoom18@gmail.com>
2024-01-05 22:44:39 +0530

    Preorder traversal

---
+++ Preorder.java
@@ -0,0 +1,26 @@
+public class Preorder {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        TreeNode current = root;
+        List<Integer> list = new ArrayList<>();
+        while (current != null) {
+            if (current.left == null) {
+                list.add(current.val);
+                current = current.right;
+            } else {
+                TreeNode predecessor = current.left;
+                while (predecessor.right != null && predecessor.right != current) {
+                    predecessor = predecessor.right;
+                }
+                if (predecessor.right == null) {
+                    predecessor.right = current;
+                    list.add(current.val);
+                    current = current.left;
+                } else {
+                    predecessor.right = null;
+                    current = current.right;
+                }
+            }
+        }
+        return list;
+    }
+}
