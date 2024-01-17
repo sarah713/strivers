7de27dfd16ff374bbbbe1f912339e41fb12c8481
Sarah <sarahkulsoom18@gmail.com>
2024-01-17 21:50:43 +0530

    queries

--- practice.sql
+++ practice.sql
@@ -21,7 +21,7 @@ where mod(id,2)=1 and DESCRIPTION !='boring'
 order by rating desc
 
 
-
+select GETDATE();
 
 select name
 from employee
@@ -66,3 +66,42 @@ from employee e1 join department d on e1.departmentID = d.id
 where 3 > (select count(distinct(e2.salary))
 from employee e2
 where e2.salary > e1.salary)
+
+
+select *
+into newtable
+from oldtable
+where 1 = 0;
+
+
+select *
+from employee e inner join salary s on e.id = p.id and e.position = 'Manager'
+
+
+select department, count(id) as empCnt
+from employee
+group by department
+order by empCnt;
+
+
+
+
+select *
+from employee e
+where exists (select *
+from employeePos p
+where e.empId = p.id);
+
+select distinct salary
+from empPosition  e1
+where 2 >= (Select count(DISTINCT salary)
+from employeePos e2
+where e1.salary >= e2.salary)
+order by e1.salary desc;
+
+select distinct salary
+from emppos e1
+where 2 >= (select count(DISTINCT salary)
+from emppos e2
+where e1.salary <= e2.salary)
+ORDER by e1.salary desc;
