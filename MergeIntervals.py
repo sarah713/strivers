41c914943f5ff7f0a6148a64d0a9269a1f13d97a
Sarah <sarahkulsoom18@gmail.com>
2024-01-22 15:56:55 +0530

    Merge intervals

---
+++ MergeIntervals.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeIntervals(self,intervals):
+        out=[]
+        for i in sorted(intervals,key=lambda i : i.start):
+            if out and i.start <= out[-1].end:
+                out[-1].end =max(out[-1].end,i.end)
+            else:
+                out += i
+        return out
