4ef5480ec65c5eff0533ad74b9e3d48a9216cd76
Sarah <sarahkulsoom18@gmail.com>
2024-01-30 13:17:22 +0530

    set matrix to zero

---
+++ SetMatrixZero.java
@@ -0,0 +1,30 @@
+
+/**
+ * SetMatrixZero
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatrixZero {
+    // brute force
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> row = new HashSet<Integer>();
+        Set<Integer> col = new HashSet<Integer>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row.contains(i) || col.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
