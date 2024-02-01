34b693dad6b21288fd2a1dc28f4ec2e9aa554949
Sarah <sarahkulsoom18@gmail.com>
2024-02-01 11:04:11 +0530

    search in matrix

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -2,7 +2,7 @@ public class SearchMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         int i = 0;
         int j = matrix.length - 1;
-        while (i < matrix.length && j < matrix[0].length) {
+        while (i < matrix.length && j >= 0) {
             if (matrix[i][j] == target) {
                 return true;
             } else if (matrix[i][j] < target) {
