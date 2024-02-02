921650f3bb1cf97e60840af0786803c50eb1de6e
Sarah <sarahkulsoom18@gmail.com>
2024-02-02 15:51:19 +0530

    Remove nth node

---
+++ practice/RemoveNthNode.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class RemoveNthNode {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
