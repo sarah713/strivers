327a36d4e67898b1ba3a26a6b87d68191943d705
Sarah <sarahkulsoom18@gmail.com>
2024-02-02 22:58:00 +0530

    Implement stack with a queue

---
+++ StackWithQueue.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackWithQueue {
+    Queue<Integer> q = new LinkedList<>();
+
+    void push(int x) {
+        q.add(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+
+    int pop() {
+        return q.remove();
+    }
+
+    int top() {
+        return q.peek();
+    }
+
+    int size() {
+        return q.size();
+    }
+}
