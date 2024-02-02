8885481a1860c41d8e30a91595655f54e265509f
Sarah <sarahkulsoom18@gmail.com>
2024-02-02 15:35:52 +0530

    Merge 2 sorted linkedlists

---
+++ practice/MergeLL.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class MergeLL {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tmp = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tmp.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                tmp.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            tmp = tmp.next;
+        }
+        while (list1 != null) {
+            tmp.next = new ListNode(list1.val);
+            list1 = list1.next;
+            tmp = tmp.next;
+        }
+        while (list2 != null) {
+            tmp.next = new ListNode(list2.val);
+            list2 = list2.next;
+            tmp = tmp.next;
+        }
+        return dummy.next;
+    }
+}
