47eb08624dbb0dc87b2c722d33abb781ad4740bd
Sarah <sarahkulsoom18@gmail.com>
2024-02-11 21:39:07 +0530

    Is Same Tree

---
+++ practice/isSameTree.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class isSameTree {
+    public boolean isSameTree(Node p, Node q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
