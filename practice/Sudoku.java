cc88e424d77ae8b0e55c7026ab4434649dce1cf0
Sarah <sarahkulsoom18@gmail.com>
2024-02-11 08:49:57 +0530

    Sudoku

---
+++ practice/Sudoku.java
@@ -0,0 +1,44 @@
+package practice;
+
+public class Sudoku {
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+
+    private boolean solve(char[][] board, int row, int col) {
+        if (row == 9) {
+            return true;
+        }
+        if (col == 9) {
+            return solve(board, row + 1, col);
+        }
+        if (board[row][col] != '.') {
+            return solve(board, row, col + 1);
+        }
+        for (char num = '1'; num <= '9'; num++) {
+            if (isValid(board, row, col, num)) {
+                board[row][col] = num;
+                if (solve(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+            if (board[i][col] == num) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
