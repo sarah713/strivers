5280bb6942ad223a4d75550a371473df976252e8
Sarah <sarahkulsoom18@gmail.com>
2024-10-18 20:47:52 +0530

    best time to buy and sell

---
+++ newpractice/BestTimeToSell.java
@@ -0,0 +1,12 @@
+package newpractice;
+
+public class BestTimeToSell {
+    public static int maximumProfit(int[] prices) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maximumProfit(prices));
+    }
+}
